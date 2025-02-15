7eb780e62b61e2ee73163ee1e4a71ab6926042db
louieeeee <louiesanqui0518@gmail.com>
2025-02-15 15:50:37 +0800

    renamed public to docs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files (HTML, CSS, JS, assets)
-app.use(express.static("public"));
+app.use(express.static("docs"));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
